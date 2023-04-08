a75416f3fc8a62193a888b81bae50bf6e81d23a9
Khai2403 <congkhai2403@gmail.com>
2023-04-08 23:22:34 +0700

    crud admin

---
+++ components/AdminHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="header">
+        <div class="header__logo">
+            <img :src="logo" alt="logo" />
+        </div>
+        <div class="header__user">
+            <header-user-avatar />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import logo from "../assets/images/Header-logo.png";
+import HeaderUserAvatar from "../features/header/components/HeaderUserAvatar.vue";
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 80px;
+    background-color: black;
+    padding: 10px 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 10;
+}
+
+.header__logo {
+    flex: 1;
+    height: 60px;
+
+    img {
+        width: 140px;
+        cursor: pointer;
+    }
+}
+
+.header__user {
+    font-size: 20px;
+    color: white;
+}
+</style>
